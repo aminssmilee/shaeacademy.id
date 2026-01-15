d2bdc14192e61f8a31920a8d3fabd7fcbff4c979
aminssmilee <salisahmad48@gmail.com>
2026-01-15 17:33:14 +0700

    Update cors

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'https://shaeacademy.vercel.app',
+        'https://shaeacademy.id'
     ],
 
     'allowed_origins_patterns' => [],
